341b9ff80c390eb2005e0e568561dabd90dd7e86
krishnamurthy-sfdx <krishnamurthyka2810@gmail.com>
2021-05-20 00:38:40 +0800

    Resume project

---
+++ force-app/main/default/lwc/resumeHeader/resumeHeader.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+import MY_PIC from '@salesforce/resourceUrl/mypic';
+
+export default class ResumeHeader extends LightningElement {
+    myPic = MY_PIC;
+    skills = [
+        {id:"1", 
+        Name:"Apex"},
+        {id:"2", 
+        Name:"SOQL"},
+        {id:"3", 
+        Name:"Visualforce"},
+        {id:"4", 
+        Name:"LWC"},
+        {id:"5", 
+        Name:"HTML"},
+        {id:"6", 
+        Name:"CSS"},
+       {id:"7", 
+    Name:"Javascript"}];
+}
